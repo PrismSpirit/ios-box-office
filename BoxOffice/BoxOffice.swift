6a3de99920b6a33d9831ac1311c420213e3f1f77
PrismSpirit <yeolkeut@gmail.com>
2024-04-02 16:32:59 +0900

    refactor: BoxOffice 모델을 extension을 활용해 nested하게 변경
    Co-authored-by: yawoong2 <adamzoong@naver.com>

--- BoxOffice/BoxOffice.swift
+++ BoxOffice/BoxOffice.swift
@@ -9,49 +9,53 @@ struct BoxOffice: Decodable {
     let boxOfficeResult: BoxOfficeResult
 }
 
-struct BoxOfficeResult: Decodable {
-    let boxOfficeType: String
-    let queriedDateRange: String
-    let dailyBoxOfficeList: [DailyBoxOffice]
-    
-    private enum CodingKeys: String, CodingKey {
-        case boxOfficeType = "boxofficeType"
-        case queriedDateRange = "showRange"
-        case dailyBoxOfficeList
+extension BoxOffice {
+    struct BoxOfficeResult: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case boxOfficeType = "boxofficeType"
+            case queriedDateRange = "showRange"
+            case dailyBoxOfficeList
+        }
+        
+        let boxOfficeType: String
+        let queriedDateRange: String
+        let dailyBoxOfficeList: [DailyBoxOffice]
     }
 }
 
-struct DailyBoxOffice: Decodable {
-    let registrationNumber, rank, rankChange: String
-    let rankOldAndNew: RankOldAndNew
-    let movieCode, movieName, openDate: String
-    let salesAmount, salesShare, salesChange, salesChangeRate, salesAccumulation: String
-    let audienceCount, audienceChange, audienceChangeRate, audienceAccmulation: String
-    let screenCount, showCount: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case registrationNumber = "rnum"
-        case rank
-        case rankChange = "rankInten"
-        case rankOldAndNew
-        case movieCode = "movieCd"
-        case movieName = "movieNm"
-        case openDate = "openDt"
-        case salesAmount = "salesAmt"
-        case salesShare = "salesShare"
-        case salesChange = "salesInten"
-        case salesChangeRate = "salesChange"
-        case salesAccumulation = "salesAcc"
-        case audienceCount = "audiCnt"
-        case audienceChange = "audiInten"
-        case audienceChangeRate = "audiChange"
-        case audienceAccmulation = "audiAcc"
-        case screenCount = "scrnCnt"
-        case showCount = "showCnt"
+extension BoxOffice.BoxOfficeResult {
+    struct DailyBoxOffice: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case registrationNumber = "rnum"
+            case rank
+            case rankChange = "rankInten"
+            case rankingEntry = "rankOldAndNew"
+            case movieCode = "movieCd"
+            case movieName = "movieNm"
+            case openDate = "openDt"
+            case salesAmount = "salesAmt"
+            case salesShare = "salesShare"
+            case salesChange = "salesInten"
+            case salesChangeRate = "salesChange"
+            case salesAccumulation = "salesAcc"
+            case audienceCount = "audiCnt"
+            case audienceChange = "audiInten"
+            case audienceChangeRate = "audiChange"
+            case audienceAccmulation = "audiAcc"
+            case screenCount = "scrnCnt"
+            case showCount = "showCnt"
+        }
+        
+        enum RankingEntry: String, Decodable {
+            case old = "OLD"
+            case new = "NEW"
+        }
+        
+        let registrationNumber, rank, rankChange: String
+        let rankingEntry: RankingEntry
+        let movieCode, movieName, openDate: String
+        let salesAmount, salesShare, salesChange, salesChangeRate, salesAccumulation: String
+        let audienceCount, audienceChange, audienceChangeRate, audienceAccmulation: String
+        let screenCount, showCount: String
     }
-}
-
-enum RankOldAndNew: String, Decodable {
-    case old = "OLD"
-    case new = "NEW"
 }
