a7ca343455dde8d8bf78a471663a62426291cf66
yawoong2 <adamzoong@naver.com>
2024-04-06 00:33:17 +0900

    refactor: getDailyBoxOffice() 메서드를 프로젝트 코딩 컨벤션에 맞춤

--- BoxOffice/NetworkService.swift
+++ BoxOffice/NetworkService.swift
@@ -25,28 +25,45 @@ final class NetworkService {
     }
     
     func getDailyBoxOffice(date: String, completion: @escaping CompletionHandler) {
-        var components = URLComponents(string: "https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json")
-        let apiKey = URLQueryItem(name: "key", value: "\(Environment.apiKey)")
-        let targetDate = URLQueryItem(name: "targetDt", value: "20240403")
-        components?.queryItems = [apiKey, targetDate]
+        let baseURL = "https://www.kobis.or.kr"
+        let path = "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+        
+        guard var components = URLComponents(string: "\(baseURL)\(path)") else {
+            completion(.failure(.invalidURL))
+            
+            return
+        }
 
-        let url = components?.url
+        components.queryItems = [URLQueryItem(name: "key", value: Environment.apiKey), URLQueryItem(name: "targetDt", value: date)]
 
-        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
-            if error != nil {
-                completion(.failure(.invalidURL))
+        guard let url = components.url else {
+            completion(.failure(.invalidURL))
+            return
+        }
+
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error {
+                let nsError = error as NSError
+                switch nsError.code {
+                case NSURLErrorTimedOut:
+                    completion(.failure(.badNetwork(error)))
+                default:
+                    completion(.failure(.requestFailure(error)))
+                }
+                return
             }
 
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
                 completion(.failure(.invalidStatusCode))
                 return
             }
 
-            guard let data else {
+            if let data {
+                completion(.success(data))
+            } else {
                 completion(.failure(.noData))
-                return
             }
-            completion(.success(data))
         }
         task.resume()
     }
