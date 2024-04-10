6657fb7d08bd1ff480fdfb0279a5070dca336ab6
PrismSpirit <yeolkeut@gmail.com>
2024-04-10 10:50:59 +0900

    feat: `DailyBoxOfficeCell` 내에서 사용할 컴포넌트 초기화

---
+++ BoxOffice/BoxOfficeListCell.swift
@@ -0,0 +1,56 @@
+//
+//  BoxOfficeListCell.swift
+//  BoxOffice
+//
+//  Created by Jaehun Lee on 4/10/24.
+//
+
+import UIKit
+
+class DailyBoxOfficeListCell: UICollectionViewListCell {
+    static let identifier = "DailyBoxOfficeListCell"
+    
+    private let rankStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private let rankLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .largeTitle)
+        return label
+    }()
+    
+    private let rankingEntryLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .callout)
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .title3)
+        return label
+    }()
+    
+    private let audienceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
+}
