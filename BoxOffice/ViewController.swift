2144cdd5d4b1760d1572d7836578bdd5f36e2b75
yawoong2 <adamzoong@naver.com>
2024-04-10 12:51:11 +0900

    feat: UICollectionView DataSource, Delegate 추가 및 컬렉션 뷰 제약조건 설정

--- BoxOffice/ViewController.swift
+++ BoxOffice/ViewController.swift
@@ -7,20 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     private let collectionView: UICollectionView = {
         let config = UICollectionLayoutListConfiguration(appearance: .plain)
         let layout = UICollectionViewCompositionalLayout.list(using: config)
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(DailyBoxOfficeListCell.self, forCellWithReuseIdentifier: "DailyBoxOfficeListCell")
         
         return collectionView
     }()
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DailyBoxOfficeListCell", for: indexPath) as? DailyBoxOfficeListCell else {
+            fatalError("Wrong cell class dequeued")
+        }
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        setConstraints()
         
     }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+    }
+    
 }
 
