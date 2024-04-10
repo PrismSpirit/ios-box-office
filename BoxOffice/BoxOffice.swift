3656cbe0fc3ee587d26f3e4d365c3e020cfea865
yawoong2 <adamzoong@naver.com>
2024-04-10 13:16:27 +0900

    feat: BoxOffice 모델 생성

---
+++ BoxOffice/BoxOffice.swift
@@ -0,0 +1,20 @@
+//
+//  BoxOffice.swift
+//  BoxOffice
+//
+//  Created by JIWOONG on 2024/04/10.
+//
+
+struct BoxOffice {
+    let rank: String
+    let rankChange: String
+    let rankingEntry: RankingEntry
+    let title: String
+    let totayAudience: String
+    let totalAudience: String
+    
+    enum RankingEntry {
+        case old
+        case new
+    }
+}
