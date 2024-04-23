734c0c81795372bc6241059e6affdea69c36a75d
PrismSpirit <yeolkeut@gmail.com>
2024-04-23 15:19:29 +0900

    feat: 이미지 URL을 받아올 타입 정의

---
+++ BoxOffice/Model/Document.swift
@@ -0,0 +1,13 @@
+//
+//  Document.swift
+//  BoxOffice
+//
+//  Created by Jaehun Lee on 4/23/24.
+//
+
+struct Document {
+    let docURL: String
+    let imageURL: String
+    let width: Int
+    let height: Int
+}
---
+++ BoxOffice/Network/DataMapping/ImageSearchResponseDTO.swift
@@ -0,0 +1,43 @@
+//
+//  ImageSearchResponseDTO.swift
+//  BoxOffice
+//
+//  Created by Jaehun Lee on 4/23/24.
+//
+
+struct ImageSearchResponseDTO: Decodable {
+    let documents: [DocumentDTO]
+}
+
+extension ImageSearchResponseDTO {
+    struct DocumentDTO: Decodable {
+        private enum CodingKeys: String, CodingKey {
+            case collection
+            case datetime
+            case displaySiteName = "display_sitename"
+            case docURL = "doc_url"
+            case imageURL = "image_url"
+            case thumbnailURL = "thumbnail_url"
+            case width
+            case height
+        }
+        
+        let collection: String
+        let datetime: String
+        let displaySiteName: String
+        let docURL: String
+        let imageURL: String
+        let thumbnailURL: String
+        let width: Int
+        let height: Int
+    }
+}
+
+extension ImageSearchResponseDTO.DocumentDTO {
+    func toModel() -> Document {
+        return .init(docURL: docURL,
+                     imageURL: imageURL,
+                     width: width,
+                     height: height)
+    }
+}
